286dedf901d876b5297f099ff3e458b45f9b0f09
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2021-07-12 19:34:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sankeyautomatic
+## ToDo:
+* legal shit
+* save sankey to local storage and add gallery tab
+* show notification on successful copy
---
+++ js/sankey-builder.js
@@ -0,0 +1,263 @@
+const lineRegex = new RegExp("(.*)\\[([0-9,.?]+)\\]\\s*(.+?)(?:\\s\\[(.+)\\])?$");
+const sankeyInput = document.getElementById("sankey-input-textarea");
+
+let sankeySvg, sankeyBox;
+let layout, diagram;
+
+const sankeyPrecisionSetting = document.getElementById("sankey-settings-precision");
+const sankeyColorpaletteSetting = document.getElementById("sankey-settings-colorscheme");
+
+const allTabs = Array.from(document.getElementById("sankey-input-tabs").getElementsByTagName("li"));
+const allTabContainers = Array.from(document.getElementById("sankey-input-box").getElementsByClassName("is-tab"));
+let currentTabIndex = 0;
+
+let inputTimer;
+sankeyInput.addEventListener("keyup", function (e) {
+    clearTimeout(inputTimer);
+    inputTimer = setTimeout(redraw, 300);
+});
+
+sankeyPrecisionSetting.addEventListener("input", function (e) {
+    processInput();
+});
+
+sankeyColorpaletteSetting.addEventListener("change", function (e) {
+    colorIndex = 0;
+    redraw();
+});
+
+document.getElementById("sankey-settings-labelabove").addEventListener("change", function (e) {
+    redraw();
+});
+
+document.getElementById("sankey-input-box").addEventListener("resize", function (e) {
+    redraw();
+});
+
+window.addEventListener("resize", processInput);
+
+document.querySelectorAll(".close-notification-button").forEach(element => {
+   element.addEventListener("click", function (e) {
+       element.parentElement.remove();
+   });
+});
+
+document.querySelectorAll(".download-as-png-button").forEach(element => {
+    element.addEventListener("click", function (e) {
+        saveSvgAsPng(d3.select("svg").node(), "sankey-builder-export", {
+            backgroundColor: "white",
+            excludeUnusedCss: true
+        });
+    });
+});
+
+document.querySelectorAll(".download-as-svg-button").forEach(element => {
+    element.addEventListener("click", function (e) {
+        saveSvg(d3.select("svg").node(), "sankey-builder-export", {
+            backgroundColor: "white",
+            excludeUnusedCss: true
+        });
+    });
+});
+
+new ClipboardJS(".copy-link-button", {
+    text: function(trigger) {
+        return window.location.href + "?content=" + serializeData();
+    }
+});
+
+document.getElementById("sankey-input-tabs").addEventListener("click", function (e) {
+    if(e.target.tagName !== "A") {
+        return;
+    }
+
+    allTabs[currentTabIndex].classList.remove("is-active");
+    allTabContainers[currentTabIndex].classList.remove("is-active");
+
+    let newTabIndex = allTabs.indexOf(e.target.parentElement);
+    allTabs[newTabIndex].classList.add("is-active");
+    allTabContainers[newTabIndex].classList.add("is-active");
+
+    currentTabIndex = newTabIndex;
+});
+
+function calculateValue(lines, originalTarget) {
+    let totalValue = 0.0;
+    lines.forEach((line, index) => {
+        if (line.startsWith("//") || line.startsWith("'")) {
+            return;
+        }
+
+        if (!lineRegex.test(line)) {
+            return;
+        }
+
+        const regexGroups = lineRegex.exec(line);
+        const source = regexGroups[1].trim();
+        let value = regexGroups[2];
+        const target = regexGroups[3].trim();
+
+        if(source === originalTarget) {
+            if(value === "?") {
+                totalValue += parseFloat(calculateValue(lines, target));
+            } else {
+                totalValue += parseFloat(value);
+            }
+            //console.log(index + ": " + originalTarget + "->" + value + "->" + target + "=" + totalValue);
+        }
+    });
+    return totalValue;
+}
+function parseInputToSankey(input) {
+    let lines = input.split("\n");
+
+    let nodeKeys = [];
+    let nodesDict = [];
+    let linksDict = [];
+
+    let precision = sankeyPrecisionSetting.value;
+
+    lines.forEach(line => {
+        if (line.startsWith("//") || line.startsWith("'")) {
+            return;
+        }
+
+        if (!lineRegex.test(line)) {
+            return;
+        }
+
+        const regexGroups = lineRegex.exec(line);
+        const source = regexGroups[1].trim();
+        let value = regexGroups[2];
+        const target = regexGroups[3].trim();
+        const color = regexGroups[4];
+
+        if (!nodeKeys.includes(source)) {
+            nodeKeys.push(source);
+            nodesDict.push({"id": source});
+        }
+
+        if (!nodeKeys.includes(target)) {
+            nodeKeys.push(target);
+            nodesDict.push({"id": target});
+        }
+
+        if (value === "?") {
+            value = calculateValue(lines, target);
+        }
+
+        linksDict.push({
+            "source": source,
+            "target": target,
+            "value": parseFloat(value).toFixed(precision),
+            "color": color in CSS3_NAMES_TO_HEX ? CSS3_NAMES_TO_HEX[color] : (color !== undefined && color.startsWith("#")) ? color : getColor(source)
+        });
+    });
+
+    return {
+        nodes: nodesDict,
+        links: linksDict
+    };
+}
+
+function redraw() {
+    let graph = parseInputToSankey(sankeyInput.value);
+    layout(graph);
+
+    sankeySvg
+        .datum(graph)
+        .datum(layout.scale(null))
+        .transition().duration(1000).ease(d3.easeCubic)
+        .call(diagram);
+}
+
+function processInput() {
+    let graph = parseInputToSankey(sankeyInput.value);
+
+    sankeyBox = document.getElementById("sankey-box");
+    sankeySvg = d3.select("#sankey-svg");
+
+    let precision = sankeyPrecisionSetting.value;
+
+    layout = d3.sankey()
+        .size([1920, 1080])
+        .linkValue(function (d) {
+            return d.value;
+        });
+
+    diagram = d3.sankeyDiagram()
+        .nodeValue(function (d) {
+            if(d.incoming.length > 0) {
+                let incomingValue = 0.0;
+                d.incoming.forEach(incomingNode => {
+                    incomingValue += parseFloat(incomingNode.value);
+                });
+
+                return incomingValue.toFixed(precision);
+            } else {
+                return d.value;
+            }
+        })
+        .nodeTitle(function (d) {
+            return d.id;
+        })
+        .nodeTooltip(function (d) {
+            let outgoingValue = 0.0, incomingValue = 0.0;
+            d.outgoing.forEach(outgoingNode => {
+                outgoingValue += parseFloat(outgoingNode.value);
+            });
+            d.incoming.forEach(incomingNode => {
+                incomingValue += parseFloat(incomingNode.value);
+            });
+
+            if(d.outgoing.length > 0 && d.incoming.length > 0 && (Math.abs(outgoingValue) !== Math.abs(incomingValue))) {
+                return d.value.toFixed(precision) + "\ndifference: " + (parseFloat(incomingValue) - parseFloat(outgoingValue)).toFixed(precision);
+            } else {
+                return d.value.toFixed(precision);
+            }
+        })
+        .linkMinWidth(function (d) {
+            return 2.5;
+        })
+        .linkColor(function (d) {
+            return d.color;
+        })
+        .margins({top: 0, right: 0, bottom: 0, left: 10});
+
+    layout(graph);
+
+    sankeySvg
+        .datum(graph)
+        .datum(layout.scale(null))
+        .transition().duration(1000).ease(d3.easeCubic)
+        .call(diagram);
+}
+
+function serializeData() {
+    return LZString.compressToBase64(sankeyInput.value);
+}
+function deserializeData(rawData) {
+    let decompressedData = LZString.decompressFromBase64(rawData);
+    sankeyInput.value = decompressedData;
+    processInput();
+    return decompressedData;
+}
+
+function findGetParameter(parameterName) {
+    let result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+if(findGetParameter("content") !== null) {
+    deserializeData(findGetParameter("content"));
+}
+
+processInput();
